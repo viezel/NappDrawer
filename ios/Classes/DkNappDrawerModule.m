3de73a4be8c8847cac97a0a51f28e6e3cc2475b9
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-01-15 14:46:28 +0100

    Remove submodule, fix statusbar-height for iPhone X, refactor meta-data

--- ios/Classes/DkNappDrawerModule.m
+++ ios/Classes/DkNappDrawerModule.m
@@ -93,48 +93,7 @@ MAKE_SYSTEM_PROP(STATUSBAR_ANIMATION_SLIDE,2);
 	[super shutdown:sender];
 }
 
-#pragma mark Cleanup 
-
--(void)dealloc
-{
-	// release any resources that have been retained by the module
-	[super dealloc];
-}
-
-#pragma mark Internal Memory Management
-
--(void)didReceiveMemoryWarning:(NSNotification*)notification
-{
-	// optionally release any resources that can be dynamically
-	// reloaded once memory is available - such as caches
-	[super didReceiveMemoryWarning:notification];
-}
-
-#pragma mark Listener Notifications
-
--(void)_listenerAdded:(NSString *)type count:(int)count
-{
-	if (count == 1 && [type isEqualToString:@"my_event"])
-	{
-		// the first (of potentially many) listener is being added 
-		// for event named 'my_event'
-	}
-}
-
--(void)_listenerRemoved:(NSString *)type count:(int)count
-{
-	if (count == 0 && [type isEqualToString:@"my_event"])
-	{
-		// the last listener called for event named 'my_event' has
-		// been removed, we can optionally clean up any resources
-		// since no body is listening at this point for that event
-	}
-}
-
-
-
 #pragma Public APIs
-
 
 -(void)hideStatusBar:(id)args
 {
@@ -145,7 +104,6 @@ MAKE_SYSTEM_PROP(STATUSBAR_ANIMATION_SLIDE,2);
     [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:style];
 }
 
-
 -(void)showStatusBar:(id)args
 {
     ENSURE_SINGLE_ARG_OR_NIL(args,NSDictionary);
@@ -154,7 +112,6 @@ MAKE_SYSTEM_PROP(STATUSBAR_ANIMATION_SLIDE,2);
     int style = [TiUtils intValue:@"animationStyle" properties:args def:UIStatusBarAnimationSlide];
     [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:style];
 }
-
 
 -(void)setStatusBarStyle:(NSNumber *)style
 {
--- ios/example/app.js
+++ ios/example/app.js
@@ -1,236 +1,255 @@
 var NappDrawerModule = require('dk.napp.drawer');
 
-function createAPIExampleWindow(){
-	var win = Ti.UI.createWindow();
-	
-	var data = [
-		{title: "Toggle shadow"},
-		{title: "Toggle stretch drawer"},
-		{title: "Close Drawer"},
-		{title: "New Window"},
-		{title: "Default Window"},
-		{title: "Remove right Drawer"}	
-	];
-	
-	var tableView = Ti.UI.createTableView({
-		data:data
-	});
-	
-	tableView.addEventListener("click", function(e){
-		Ti.API.info("isLeftWindowOpen: " + drawer.isLeftWindowOpen());
-		switch(e.index){
-			case 0:
-				drawer.setShowShadow(!drawer.showShadow);
-				break;
-			case 1:
-				drawer.setShouldStretchDrawer(!drawer.shouldStretchDrawer);
-				break;
-			case 2:
-				drawer.toggleLeftWindow();
-				break;
-			case 3:
-				var newWin = openNewNavWindow();
-				drawer.setCenterWindow(newWin);
-				drawer.toggleLeftWindow();
-				break;
-			case 4:
-				drawer.setCenterWindow(createCenterNavWindow());
-				drawer.toggleLeftWindow();
-				break;
-			case 5:
-				drawer.setRightWindow(false);
-				drawer.toggleLeftWindow();
-				break;
-		}
-	});
-	
-	win.add(tableView);
-	return win;
+function createAPIExampleWindow() {
+  var win = Ti.UI.createWindow();
+
+  var data = [{
+      title: "Toggle shadow"
+    },
+    {
+      title: "Toggle stretch drawer"
+    },
+    {
+      title: "Close Drawer"
+    },
+    {
+      title: "New Window"
+    },
+    {
+      title: "Default Window"
+    },
+    {
+      title: "Remove right Drawer"
+    }
+  ];
+
+  var tableView = Ti.UI.createTableView({
+    data: data
+  });
+
+  tableView.addEventListener("click", function(e) {
+    Ti.API.info("isLeftWindowOpen: " + drawer.isLeftWindowOpen());
+    switch (e.index) {
+      case 0:
+        drawer.setShowShadow(!drawer.showShadow);
+        break;
+      case 1:
+        drawer.setShouldStretchDrawer(!drawer.shouldStretchDrawer);
+        break;
+      case 2:
+        drawer.toggleLeftWindow();
+        break;
+      case 3:
+        var newWin = openNewNavWindow();
+        drawer.setCenterWindow(newWin);
+        drawer.toggleLeftWindow();
+        break;
+      case 4:
+        drawer.setCenterWindow(createCenterNavWindow());
+        drawer.toggleLeftWindow();
+        break;
+      case 5:
+        drawer.setRightWindow(false);
+        drawer.toggleLeftWindow();
+        break;
+    }
+  });
+
+  win.add(tableView);
+  return win;
 }
 
 
-function openNewNavWindow(){
-	var leftBtn = Ti.UI.createButton({title:"Left"});
-	leftBtn.addEventListener("click", function(){
-		drawer.toggleLeftWindow();
-	});
-	var win = Ti.UI.createWindow({
-		backgroundColor:'#222',
-		translucent:true,
-		extendEdges:[Ti.UI.EXTEND_EDGE_TOP],
-		title:"New Nav Window",
-		barColor:"#FFA",
-		tintColor:"yellow",
-		leftNavButton:leftBtn
-	});
-	
-	var scrollView = Ti.UI.createScrollView({
-		layout:"vertical",
-		left:0,
-		right:0,
-		contentHeight:'auto',
-		contentWidth:"100%",
-		showVerticalScrollIndicator: true,
-		showHorizontalScrollIndicator: false
-	});
-	
-	for(var i=0; i<20; i++){
-		var label = Ti.UI.createLabel({
-			top:30,
-			text:"iOS7 is the new black",
-			color:"#FFF",
-			font:{
-				fontSize:20
-			}
-		});
-		scrollView.add(label);
-	}
-	win.add(scrollView);
-	var navController =  Ti.UI.iOS.createNavigationWindow({
-		window : win
-	});
-	return navController;
-}
+function openNewNavWindow() {
+  var leftBtn = Ti.UI.createButton({
+    title: "Left"
+  });
+  leftBtn.addEventListener("click", function() {
+    drawer.toggleLeftWindow();
+  });
+  var win = Ti.UI.createWindow({
+    backgroundColor: '#222',
+    translucent: true,
+    extendEdges: [Ti.UI.EXTEND_EDGE_TOP],
+    title: "New Nav Window",
+    barColor: "#FFA",
+    tintColor: "yellow",
+    leftNavButton: leftBtn
+  });
 
+  var scrollView = Ti.UI.createScrollView({
+    layout: "vertical",
+    left: 0,
+    right: 0,
+    contentHeight: 'auto',
+    contentWidth: "100%",
+    showVerticalScrollIndicator: true,
+    showHorizontalScrollIndicator: false
+  });
 
-function createCenterNavWindow(){	
-	var leftBtn = Ti.UI.createButton({title:"Left"});
-	leftBtn.addEventListener("click", function(){
-		drawer.toggleLeftWindow();
-	});
-	var rightBtn = Ti.UI.createButton({title:"Right"});
-	rightBtn.addEventListener("click", function(){
-		drawer.toggleRightWindow();
-	});
-	
-	var win = Ti.UI.createWindow({
-		backgroundColor:'#eee',
-		translucent:false,
-		title:"NappDrawer",
-		barColor:"#F9A",
-		tintColor:"purple",
-		leftNavButton: leftBtn,
-		rightNavButton: rightBtn
-	});
-	
-	var closeGestureMode = 1;
-	var closeGestureModeBtn = Ti.UI.createButton({
-		title:"closeGestureMode: ALL", 
-		width:300,
-		top:80
-	});
-	closeGestureModeBtn.addEventListener("click", function(e){
-		if(closeGestureMode == 2){
-			closeGestureMode = 0;
-		} else {
-			closeGestureMode++;
-		}
-		switch(closeGestureMode){
-			case 0:
-				drawer.setCloseDrawerGestureMode(NappDrawerModule.CLOSE_MODE_TAP_CENTERWINDOW);
-				closeGestureModeBtn.setTitle("closeGesture: Tap Center");
-				break;
-			case 1:
-				drawer.setCloseDrawerGestureMode(NappDrawerModule.CLOSE_MODE_ALL);
-				closeGestureModeBtn.setTitle("closeGesture: ALL");
-				break;
-			case 2:
-				drawer.setCloseDrawerGestureMode(NappDrawerModule.CLOSE_MODE_PANNING_NAVBAR);
-				closeGestureModeBtn.setTitle("closeGesture: NAVBAR");
-				break;
-		}
-	});
-	win.add(closeGestureModeBtn);
-	
-	
-	var animationMode = 0;
-	var animationModeBtn = Ti.UI.createButton({
-		title:"animation: NONE", 
-		width:300,
-		top:140
-	});
-	animationModeBtn.addEventListener("click", function(e){
-		if(animationMode == 5){
-			animationMode = 0;
-		} else {
-			animationMode++;
-		}
-		switch(animationMode){
-			case 0:
-				drawer.setAnimationMode(NappDrawerModule.ANIMATION_NONE);
-				animationModeBtn.setTitle("animation: None");
-				break;
-			case 1:
-				drawer.setAnimationMode(NappDrawerModule.ANIMATION_PARALLAX_FACTOR_3);
-				animationModeBtn.setTitle("animation: Parallax factor 3");
-				break;
-			case 2:
-				drawer.setAnimationMode(NappDrawerModule.ANIMATION_PARALLAX_FACTOR_7);
-				animationModeBtn.setTitle("animation: Parallax factor 7");
-				break;
-			case 3:
-				drawer.setAnimationMode(NappDrawerModule.ANIMATION_FADE);
-				animationModeBtn.setTitle("animation: Fade");
-				break;
-			case 4:
-				drawer.setAnimationMode(NappDrawerModule.ANIMATION_SLIDE);
-				animationModeBtn.setTitle("animation: Slide");
-				break;
-			case 5:
-				drawer.setAnimationMode(NappDrawerModule.ANIMATION_SLIDE_SCALE);
-				animationModeBtn.setTitle("animation: Slide & Scale");
-				break;
-		}
-	});
-	win.add(animationModeBtn);
-	
-	
-	var slider = Ti.UI.createSlider({
-		top: 280,
-	    min: 50,
-	    max: 280,
-	    width: 280,
-	    value: 200
-	});
-	var label = Ti.UI.createLabel({
-	    text: "Left Drawer Width: " + slider.value,
-	    top: 250
+  for (var i = 0; i < 20; i++) {
+    var label = Ti.UI.createLabel({
+      top: 30,
+      text: "iOS7 is the new black",
+      color: "#FFF",
+      font: {
+        fontSize: 20
+      }
     });
-	slider.addEventListener('touchend', function(e) {
-		var value = Math.round(e.value);
-		label.setText("Left Drawer Width: " + value);
-	    drawer.setLeftDrawerWidth(value);
-	});
-	win.add(label);
-	win.add(slider);
+    scrollView.add(label);
+  }
+  win.add(scrollView);
+  var navController = Ti.UI.iOS.createNavigationWindow({
+    window: win
+  });
+  return navController;
+}
 
-	var navController =  Ti.UI.iOS.createNavigationWindow({
-		window : win
-	});
-	return navController;
+
+function createCenterNavWindow() {
+  var leftBtn = Ti.UI.createButton({
+    title: "Left"
+  });
+  leftBtn.addEventListener("click", function() {
+    drawer.toggleLeftWindow();
+  });
+  var rightBtn = Ti.UI.createButton({
+    title: "Right"
+  });
+  rightBtn.addEventListener("click", function() {
+    drawer.toggleRightWindow();
+  });
+
+  var win = Ti.UI.createWindow({
+    backgroundColor: '#eee',
+    translucent: false,
+    title: "NappDrawer",
+    barColor: "#F9A",
+    tintColor: "purple",
+    leftNavButton: leftBtn,
+    rightNavButton: rightBtn
+  });
+
+  var closeGestureMode = 1;
+  var closeGestureModeBtn = Ti.UI.createButton({
+    title: "closeGestureMode: ALL",
+    width: 300,
+    top: 80
+  });
+  closeGestureModeBtn.addEventListener("click", function(e) {
+    if (closeGestureMode == 2) {
+      closeGestureMode = 0;
+    } else {
+      closeGestureMode++;
+    }
+    switch (closeGestureMode) {
+      case 0:
+        drawer.setCloseDrawerGestureMode(NappDrawerModule.CLOSE_MODE_TAP_CENTERWINDOW);
+        closeGestureModeBtn.setTitle("closeGesture: Tap Center");
+        break;
+      case 1:
+        drawer.setCloseDrawerGestureMode(NappDrawerModule.CLOSE_MODE_ALL);
+        closeGestureModeBtn.setTitle("closeGesture: ALL");
+        break;
+      case 2:
+        drawer.setCloseDrawerGestureMode(NappDrawerModule.CLOSE_MODE_PANNING_NAVBAR);
+        closeGestureModeBtn.setTitle("closeGesture: NAVBAR");
+        break;
+    }
+  });
+  win.add(closeGestureModeBtn);
+
+
+  var animationMode = 0;
+  var animationModeBtn = Ti.UI.createButton({
+    title: "animation: NONE",
+    width: 300,
+    top: 140
+  });
+  animationModeBtn.addEventListener("click", function(e) {
+    if (animationMode == 5) {
+      animationMode = 0;
+    } else {
+      animationMode++;
+    }
+    switch (animationMode) {
+      case 0:
+        drawer.setAnimationMode(NappDrawerModule.ANIMATION_NONE);
+        animationModeBtn.setTitle("animation: None");
+        break;
+      case 1:
+        drawer.setAnimationMode(NappDrawerModule.ANIMATION_PARALLAX_FACTOR_3);
+        animationModeBtn.setTitle("animation: Parallax factor 3");
+        break;
+      case 2:
+        drawer.setAnimationMode(NappDrawerModule.ANIMATION_PARALLAX_FACTOR_7);
+        animationModeBtn.setTitle("animation: Parallax factor 7");
+        break;
+      case 3:
+        drawer.setAnimationMode(NappDrawerModule.ANIMATION_FADE);
+        animationModeBtn.setTitle("animation: Fade");
+        break;
+      case 4:
+        drawer.setAnimationMode(NappDrawerModule.ANIMATION_SLIDE);
+        animationModeBtn.setTitle("animation: Slide");
+        break;
+      case 5:
+        drawer.setAnimationMode(NappDrawerModule.ANIMATION_SLIDE_SCALE);
+        animationModeBtn.setTitle("animation: Slide & Scale");
+        break;
+    }
+  });
+  win.add(animationModeBtn);
+
+
+  var slider = Ti.UI.createSlider({
+    top: 280,
+    min: 50,
+    max: 280,
+    width: 280,
+    value: 200
+  });
+  var label = Ti.UI.createLabel({
+    text: "Left Drawer Width: " + slider.value,
+    top: 250
+  });
+  slider.addEventListener('touchend', function(e) {
+    var value = Math.round(e.value);
+    label.setText("Left Drawer Width: " + value);
+    drawer.setLeftDrawerWidth(value);
+  });
+  win.add(label);
+  win.add(slider);
+
+  var navController = Ti.UI.iOS.createNavigationWindow({
+    window: win
+  });
+  return navController;
 }
 
 var mainWindow = createCenterNavWindow();
 
 var drawer = NappDrawerModule.createDrawer({
-	leftWindow: createAPIExampleWindow(),
-	centerWindow: mainWindow,
-	rightWindow: Ti.UI.createWindow({backgroundColor:"#FFF"}),
-	closeDrawerGestureMode: NappDrawerModule.CLOSE_MODE_ALL,
-	openDrawerGestureMode: NappDrawerModule.OPEN_MODE_ALL,
-	showShadow: false, //no shadow in iOS7
-	leftDrawerWidth: 200,
-	rightDrawerWidth: 120,
-	statusBarStyle: NappDrawerModule.STATUSBAR_WHITE,  // remember to set UIViewControllerBasedStatusBarAppearance to false in tiapp.xml
-	orientationModes: [Ti.UI.PORTRAIT, Ti.UI.UPSIDE_PORTRAIT]
+  leftWindow: createAPIExampleWindow(),
+  centerWindow: mainWindow,
+  rightWindow: Ti.UI.createWindow({
+    backgroundColor: "#FFF"
+  }),
+  closeDrawerGestureMode: NappDrawerModule.CLOSE_MODE_ALL,
+  openDrawerGestureMode: NappDrawerModule.OPEN_MODE_ALL,
+  showShadow: false, //no shadow in iOS7
+  leftDrawerWidth: 200,
+  rightDrawerWidth: 120,
+  statusBarStyle: NappDrawerModule.STATUSBAR_WHITE, // remember to set UIViewControllerBasedStatusBarAppearance to false in tiapp.xml
+  orientationModes: [Ti.UI.PORTRAIT, Ti.UI.UPSIDE_PORTRAIT]
 });
 
 drawer.addEventListener('windowDidOpen', function(e) {
-	Ti.API.info("windowDidOpen");
+  Ti.API.info("windowDidOpen");
 });
 
 drawer.addEventListener('windowDidClose', function(e) {
-	Ti.API.info("windowDidClose");
+  Ti.API.info("windowDidClose");
 });
 
 drawer.open();
